b7eb1af28d9f454facb72bf2015f7089e5b1e1a7
DiogoZ <diogozang2@gmail.com>
2021-10-23 22:11:23 -0300

    unused components removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,4 @@
-import { SettingsComponent } from './views/settings/settings.component';
-import { MapsComponent } from './views/maps/maps.component';
 import { StoryComponent } from './views/story/story.component';
-import { AboutherComponent } from './views/abouther/abouther.component';
-import { AbouthimComponent } from './views/abouthim/abouthim.component';
-import { DonateComponent } from './views/donate/donate.component';
 import { HomeComponent } from './views/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -16,42 +11,14 @@ const routes: Routes = [
     component:  HomeComponent
   },
   {
-    path: "abouthim",
-    component: AbouthimComponent
-  },
-  {
-    path: "abouther",
-    component: AboutherComponent
-  },
-  {
     path: "story",
     component: StoryComponent
-  },
-  {
-    path: "map",
-    component: MapsComponent
-  },
-  {
-    path: "settings",
-    component: SettingsComponent
-  },
-  {
-    path: "gifts",
-    component: DonateComponent
   },
   {
     path: "pictures",
     component: PicturesComponent
   }
 ];
-
-// <li routerlink="/">home</li>
-//         <li routerlink="abouther">about her</li>
-//         <li routerlink="abouthim">about him</li>
-//         <li routerlink="story">our story</li>
-//         <li routerlink="map">maps</li>
-//         <li routerlink="donate">donate</li>
-//         <li routerlink="settings">settings</li>
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,6 @@ import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './views/home/home.component';
 import { StoryComponent } from './views/story/story.component';
-import { AboutherComponent } from './views/abouther/abouther.component';
-import { AbouthimComponent } from './views/abouthim/abouthim.component';
-import { MapsComponent } from './views/maps/maps.component';
-import { DonateComponent } from './views/donate/donate.component';
-import { SettingsComponent } from './views/settings/settings.component';
 import { PicturesComponent } from './views/pictures/pictures.component';
 import { CarouselComponent } from './views/pictures/carousel/carousel.component';
 
@@ -23,11 +18,6 @@ import { CarouselComponent } from './views/pictures/carousel/carousel.component'
     FooterComponent,
     HomeComponent,
     StoryComponent,
-    AboutherComponent,
-    AbouthimComponent,
-    MapsComponent,
-    DonateComponent,
-    SettingsComponent,
     PicturesComponent,
     CarouselComponent,
   ],
