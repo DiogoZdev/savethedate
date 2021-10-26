04972a956c8a0a36e43b2e74bdd14802e7d9a271
DiogoZ <diogozang2@gmail.com>
2021-10-25 22:10:09 -0300

    mobile menu implemented

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -5,11 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
+export class NavComponent {
+
+  public showMenu = false;
 
   constructor() { }
 
-  ngOnInit(): void {
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
   }
+
+
 
 }
